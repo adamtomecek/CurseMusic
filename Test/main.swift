da123f2831196e0aa04d27221ff12c2b7caa3a3b
Adam Tomeček <adam.tomecek@gmail.com>
2016-05-11 14:29:22 +0200

    Initial Commit

---
+++ Test/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Test
+//
+//  Created by Adam Tomecek on 11/05/16.
+//  Copyright © 2016 Adam Tomecek. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
